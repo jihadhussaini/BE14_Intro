fb80c56969b80170f01c7d71d73b42651f6f383e
Jihad Hussaini <86664752+jihadhussaini@users.noreply.github.com>
2021-08-23 10:08:38 +0700

    Task Intro is submitted

---
+++ first.js
@@ -0,0 +1,13 @@
+let teks = "Halo";
+var nama =  "Jihad";
+console.log(teks + " " + nama);
+
+const kelas = 10;
+console.log("Kelas: " + kelas);
+
+const array = ["Rizky", 10, {nama: "Rizky", kelas: 10}];
+console.log(array);
+
+const objek = {kelas: ["frontend", "backend", "mobile"]};
+console.log(objek);
+console.log(objek.kelas[2]);
