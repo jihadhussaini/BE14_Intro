dae6456cfed85592a759f63d5be9a74ae84f6487
Jihad Hussaini <86664752+jihadhussaini@users.noreply.github.com>
2021-08-23 11:53:06 +0700

    Task intro done

--- first.js
+++ first.js
@@ -2,10 +2,10 @@ let teks = "Halo";
 var nama =  "Jihad";
 console.log(teks + " " + nama);
 
-const kelas = 10;
+const kelas = 10
 console.log("Kelas: " + kelas);
 
-const array = ["Rizky", 10, {nama: "Rizky", kelas: 10}];
+const array = ["Rizky", 10, {nama: "Rizky", kelas: 10}]
 console.log(array);
 
 const objek = {kelas: ["frontend", "backend", "mobile"]};
